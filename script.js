7c6a9345c6cf202e5a11c04fd7c26f6153ba8c62
Saki9891 <saikirankollimalla@gmail.com>
2024-01-09 14:42:28 +0530

    files added

---
+++ script.js
@@ -0,0 +1,24 @@
+// n=prompt('enter a value')
+// for(let i =1; i<=n;i++){
+//     console.log(i)
+// }
+
+// n=prompt('enter a value')
+// result =prompt('enter a value')
+// for(let i =1;i<=10;i++){
+//     console.log(`${result}*${i}=${result*i}`)
+
+// }
+
+// n=prompt('enter a value:')
+// sum=prompt('enter a value:')
+// for(let i =0;i<=n;i++){
+//     sum=sum+i
+//     console.log(sum)
+// }
+n=prompt('enter a value')
+fact=1
+for(let i =1;i<=n;i++){
+    fact = fact*i
+    console.log(`factorial of n is ${fact}`)
+}
